ad5a7921a17c64dafe07ecf58a2f69e333779f19
Alexander <akochnev66@gmail.com>
2026-02-21 23:13:29 +0300

    Текущая ветка: multy  Изменения, которые будут включены в коммит: 	новый файл:    mg_testrestore.sh

---
+++ mg_testrestore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# восстановление mongodb
+BACKUP_NAME="mg_backup.gz"
+
+# docker exec -i имя_контейнера mongorestore --archive --gzip --db имя_базы < backup.gz
+# docker exec -i mongo mongorestore --archive --gzip --nsInclude='wine_database.*' < backup/$BACKUP_NAME
+docker exec -i mongo_test mongorestore \
+  --username=admin \
+  --password=admin \
+  --authenticationDatabase=admin \
+  --archive \
+  --gzip \
+  --nsInclude="wine_database.*" < backup/mg_backup.gz
