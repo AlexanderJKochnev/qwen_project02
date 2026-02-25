87616807ad4562036e056a2b2196b891b16dde74
Alexander <akochnev66@gmail.com>
2026-02-26 00:56:09 +0300

    ammended

--- preact_front/src/config/api.ts
+++ preact_front/src/config/api.ts
@@ -1,9 +1,9 @@
 // src/config/api.ts
 
 // Определяем базовый URL в зависимости от среды
-export const API_BASE_URL = import.meta.env.DEV
-  ? '/proxy-api' // Для разработки (через Vite proxy) будет проксироваться через Vite → http://app:8091
-  : 'https://api.abc8888.ru'; // Для продакшена (через Nginx шлюз)
+// export const API_BASE_URL = import.meta.env.DEV
+//  ? '/proxy-api' // Для разработки (через Vite proxy) будет проксироваться через Vite → http://app:8091
+//  : 'https://api.abc8888.ru'; // Для продакшена (через Nginx шлюз)
 
-// export const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const IMAGE_BASE_URL = `${API_BASE_URL}`;
