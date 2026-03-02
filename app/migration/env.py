36bd61183dbb9836d704825b8a7397b29b24690d
Alexander <akochnev66@gmail.com>
2026-03-02 08:27:09 +0300

    ammend

--- app/migration/env.py
+++ app/migration/env.py
@@ -31,6 +31,7 @@ from app.support.subregion.model import Subregion
 from app.support.subcategory.model import Subcategory
 from app.support.parser.model import Name, Image, Code, Rawdata, Registry
 from app.support.field_keys.model import FieldKey
+from app.support.ollama.model import Ollama
 
 
 # this is the Alembic Config object, which provides
