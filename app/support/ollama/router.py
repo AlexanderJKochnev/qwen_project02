ca7f0ba4f6f914de06652060e1b1a7203dd8d19f
Alexander <akochnev66@gmail.com>
2026-03-02 16:03:19 +0300

    ammend

--- app/support/ollama/router.py
+++ app/support/ollama/router.py
@@ -1,12 +1,12 @@
 # app.suport.ollama.router.py
 from typing import List
-
+from loguru import logger
 from fastapi import BackgroundTasks, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.config.database.db_async import get_db
 from app.core.routers.base import BaseRouter
-from app.core.utils.common_utils import compare_lists_compact
+from app.core.utils.common_utils import compare_lists_compact, jprint
 from app.support.ollama.model import Ollama
 from app.support.ollama.schemas import LlmResponseSchema, OllamaCreate, OllamaRead, OllamaUpdate
 from app.support.ollama.service import LLMService
@@ -30,23 +30,27 @@ class OllamaRouter(BaseRouter):
         """
         try:
             response: List[dict] = await self.LLMservice.get_models_list()
-            result = [LlmResponseSchema.model_validate(key).model_dump() for key in response]
-            from app.core.utils.common_utils import jprint
-            result2 = await self.service.get_full(self.repo, self.model, session)
-            result3 = (OllamaCreate(**key.to_dict()).model_dump() for key in result2)
-            #  словарь с различиями added, removed, changed
-            resp = compare_lists_compact(result3, result, 'model')
-            # для remove <и changed> заменяем на id
-            for key in ('removed', 'changed'):
-                if x := resp.get(key):
-                    x = [b if key == 'removed' else (b, a)
-                         for a in x for b in result2 if a['model'] == b.model]
-                    resp[key] = x
+            iter = 0
+            while iter < 2:
+                iter += 1
+                result = [LlmResponseSchema.model_validate(key).model_dump() for key in response]
+                result2 = await self.service.get_full(self.repo, self.model, session)
+                result3 = (OllamaCreate(**key.to_dict()).model_dump() for key in result2)
+                #  словарь с различиями added, removed, changed
+                resp = compare_lists_compact(result3, result, 'model')
+                if not resp:
+                    return result
+                # для remove <и changed> заменяем на id
+                for key in ('removed', 'changed'):
+                    if x := resp.get(key):
+                        x = [b if key == 'removed' else (b, a)
+                             for a in x for b in result2 if a['model'] == b.model]
+                        resp[key] = x
+                await self.service.maintain_llm_database(resp, self.repo, self.model, session)
+            error = 'база данных ll моделей не может синхронизироваться с реально загруженными ll моделямии'
+            logger.error(error)
             jprint(resp)
-
-            await self.service.maintain_llm_database(resp, self.repo, self.model, session)
-            jprint(result2)
-            return result
+            raise Exception(error)
         except Exception as e:
             raise HTTPException(status_code=501, detail=e)
 
