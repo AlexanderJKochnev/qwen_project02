79df86673faebf995b67855b1d1936dfb559d372
Alexander <akochnev66@gmail.com>
2026-03-08 01:19:48 +0300

    ammend

--- app/core/enum.py
+++ app/core/enum.py
@@ -1,5 +1,6 @@
 # app.core.enum.py
 from enum import Enum
+from sqlalchemy import select
 from app.core.config.database.db_sync import SessionLocalSync
 from app.support.ollama.model import Ollama, Prompt, Proption
 
@@ -10,15 +11,15 @@ def fetch_all_startup_data() -> dict:
     # session.query(LlmModel.name)
     with SessionLocalSync() as session:
         # Запрос 1: Модели
-        models = session.query(Ollama.model).order_by(Ollama.model.asc()).scalars().all()
+        models = session.scalars(select(Ollama.model).order_by(Ollama.model.asc()).scalars()).all()
         # models = session.execute(text("SELECT name FROM llm_models")).scalars().all()
         data['models'] = [m for m in models] or ["qwen3:8b"]
         # Запрос 2: Prompt
-        prompts = session.query(Prompt.role).order_by(Prompt.role.asc()).scalars().all()
+        prompts = session.scalars(select(Prompt.role).order_by(Prompt.role.asc()).scalars()).all()
         data['prompts'] = [pr for pr in prompts] or ["translator"]
 
         # Запрос 3: Preset
-        presets = session.query(Proption.preset).order_by(Proption.preset.asc()).scalars().all()
+        presets = session.scalars(select(Proption.preset).order_by(Proption.preset.asc()).scalars()).all()
         data['presets'] = [c for c in presets] or ["balanced"]
 
     return data
