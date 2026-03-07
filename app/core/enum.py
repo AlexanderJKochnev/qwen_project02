a45201bedc5dda5420afd51a3b0ee9fea1ad0c94
Alexander <akochnev66@gmail.com>
2026-03-08 01:21:31 +0300

    ammend

--- app/core/enum.py
+++ app/core/enum.py
@@ -11,15 +11,15 @@ def fetch_all_startup_data() -> dict:
     # session.query(LlmModel.name)
     with SessionLocalSync() as session:
         # Запрос 1: Модели
-        models = session.scalars(select(Ollama.model).order_by(Ollama.model.asc()).scalars()).all()
+        models = session.scalars(select(Ollama.model).order_by(Ollama.model.asc())).all()
         # models = session.execute(text("SELECT name FROM llm_models")).scalars().all()
         data['models'] = [m for m in models] or ["qwen3:8b"]
         # Запрос 2: Prompt
-        prompts = session.scalars(select(Prompt.role).order_by(Prompt.role.asc()).scalars()).all()
+        prompts = session.scalars(select(Prompt.role).order_by(Prompt.role.asc())).all()
         data['prompts'] = [pr for pr in prompts] or ["translator"]
 
         # Запрос 3: Preset
-        presets = session.scalars(select(Proption.preset).order_by(Proption.preset.asc()).scalars()).all()
+        presets = session.scalars(select(Proption.preset).order_by(Proption.preset.asc())).all()
         data['presets'] = [c for c in presets] or ["balanced"]
 
     return data
