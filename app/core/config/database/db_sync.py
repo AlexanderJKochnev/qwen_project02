fc937c5a632fe8a17b45a90bb81894b79b5e0418
Alexander <akochnev66@gmail.com>
2026-03-08 01:15:56 +0300

    ammend

--- app/core/config/database/db_sync.py
+++ app/core/config/database/db_sync.py
@@ -3,7 +3,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config.database.db_config import settings_db
-from app.support.ollama.model import Proption, Prompt, Ollama
 
 # 0. url для подключения
 sync_database_url = settings_db.database_url.replace("postgresql+asyncpg://", "postgresql://")
@@ -17,23 +16,3 @@ engine_sync = create_engine(
 
 # 2. Синхронная фабрика сессий
 SessionLocalSync = sessionmaker(autocommit=False, autoflush=False, bind=engine_sync)
-
-
-# 3. Функция для получения списка строк
-def fetch_all_startup_data() -> dict:
-    data = {}
-    # session.query(LlmModel.name)
-    with SessionLocalSync() as session:
-        # Запрос 1: Модели
-        models = session.query(Ollama.model).scalar().all()
-        # models = session.execute(text("SELECT name FROM llm_models")).scalars().all()
-        data['models'] = [m for m in models] or ["qwen3:8b"]
-        # Запрос 2: Prompt
-        prompts = session.query(Prompt.role).scalar().all()
-        data['prompts'] = [pr for pr in prompts] or ["translator"]
-
-        # Запрос 3: Preset
-        presets = session.query(Proption.preset).scalar().all()
-        data['presets'] = [c for c in presets] or ["balanced"]
-
-    return data
