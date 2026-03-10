4b62e4d5767803fa28a7d3c1da6e50d0bc09c93a
Alexander <akochnev66@gmail.com>
2026-03-10 10:31:55 +0300

    lwin_import.sh added  Текущая ветка: interim2  Изменения, которые будут включены в коммит: 	новый файл:    lwin_import.sh

---
+++ lwin_import.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Имя сервиса из docker-compose
+SERVICE_NAME="wine_host"
+DB_NAME="wine_db"
+# Имя пользователя БД (замените, если отличается)
+DB_USER="wine"
+# Имя файла
+# cat /путь/к/файлу.csv | docker exec -i имя_контейнера psql -U имя_пользователя -d имя_базы -c "\copy имя_таблицы FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ';')"
+# BACKUP_NAME="pg_backup_$(date +%Y%m%d_%H%M%S).sql"
+FILE_NAME="/upload_volume/LWIN_clear.txt"
+
+echo "--- Начинаю импорт lwin в контейнер $SERVICE_NAME ---"
+
+# Создаем дамп
+cat $FILE_NAME | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t')"
+
+if [ $? -eq 0 ]; then
+    echo "--- импорт успешно сделан ---"
+else
+    echo "Ошибка при создании импорта LWIN"
+    exit 1
+fi
