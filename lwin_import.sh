3c5a90da7f61849184d97e0fa8eeadd66a77c866
Alexander <akochnev66@gmail.com>
2026-03-10 11:13:30 +0300

    ammend

--- lwin_import.sh
+++ lwin_import.sh
@@ -5,6 +5,8 @@ SERVICE_NAME="test-wine_host-1"
 DB_NAME="wine_db"
 # Имя пользователя БД (замените, если отличается)
 DB_USER="wine"
+COLUMNS="(LWIN, STATUS, DISPLAY_NAME, PRODUCER_TITLE, PRODUCER_NAME, WINE, COUNTRY, REGION, SUB_REGION, SITE, PARCEL, COLOUR, TYPE, SUB_TYPE, DESIGNATION, CLASSIFICATION, VINTAGE_CONFIG, FIRST_VINTAGE, FINAL_VINTAGE, DATE_ADDED, DATE_UPDATED)"
+# COLUMNS="(lwin, status, display_name, producer_title, producer_name, wine, country, region, sub_region, site, parcel, colour, type, sub_type, designation, classification, vintage_config, first_vintage, final_vintage, date_added, date_updated)"
 # Имя файла
 # cat /путь/к/файлу.csv | docker exec -i имя_контейнера psql -U имя_пользователя -d имя_базы -c "\copy имя_таблицы FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ';')"
 # BACKUP_NAME="pg_backup_$(date +%Y%m%d_%H%M%S).sql"
@@ -13,9 +15,9 @@ FILE_NAME="upload_volume/LWIN_clear.txt"
 echo "--- Начинаю импорт lwin в контейнер $SERVICE_NAME ---"
 
 # Создаем дамп
-# cat $FILE_NAME | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins (LWIN, STATUS, DISPLAY_NAME, PRODUCER_TITLE, PRODUCER_NAME, WINE, COUNTRY, REGION, SUB_REGION, SITE, PARCEL, COLOUR, TYPE, SUB_TYPE, DESIGNATION, CLASSIFICATION, VINTAGE_CONFIG, FIRST_VINTAGE, FINAL_VINTAGE, DATE_ADDED, DATE_UPDATED) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b', NULL '')"
+# cat $FILE_NAME | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins  FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b', NULL '')"
 
-cat $FILE_NAME | sed 's/\r//g' | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins (lwin, status, display_name, producer_title, producer_name, wine, country, region, sub_region, site, parcel, colour, type, sub_type, designation, classification, vintage_config, first_vintage, final_vintage, date_added, date_updated) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b')"
+cat $FILE_NAME | sed 's/\r//g' | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins $COLUMNS FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b')"
 
 
 if [ $? -eq 0 ]; then
