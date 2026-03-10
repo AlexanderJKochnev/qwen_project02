fd586b873348ea551d6dc640789aaf65ffc28413
Alexander <akochnev66@gmail.com>
2026-03-10 16:41:30 +0300

    ammend

--- lwin_import.sh
+++ lwin_import.sh
@@ -15,12 +15,12 @@ FILE_NAME="upload_volume/LWIN_clear.tsv"
 echo "--- Начинаю импорт lwin в контейнер $SERVICE_NAME ---"
 
 # Создаем дамп
-# cat $FILE_NAME | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins  FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b', NULL '')"
+cat $FILE_NAME | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins  FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b')"
 
 # cat $FILE_NAME | sed 's/\r//g' | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME -c "\copy lwins $COLUMNS FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b', ENCODING 'WIN1251')"
 
-cat $FILE_NAME | sed 's/\r//g' | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME \
- -c "SET datestyle = 'ISO, DMY'; \copy lwins $COLUMNS FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b')"
+# cat $FILE_NAME | sed 's/\r//g' | docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME \
+#  -c "SET datestyle = 'ISO, DMY'; \copy lwins $COLUMNS FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER E'\t', QUOTE E'\b')"
 
 # (echo "SET datestyle = 'ISO, DMY';"; cat $FILE_NAME | sed 's/\r//g') | \
 # docker exec -i $SERVICE_NAME psql -U $DB_USER -d $DB_NAME \
