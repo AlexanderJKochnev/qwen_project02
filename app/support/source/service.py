cfe271b6151fb9d980bb6c1e7699db700eed78af
Alexander <akochnev66@gmail.com>
2026-03-14 02:28:28 +0300

    ammend

--- app/support/source/service.py
+++ app/support/source/service.py
@@ -1,6 +1,7 @@
 # app.support.source.servic.py
 
 from app.core.services.service import Service
+from app.support.source.repository import SourceRepository  # NOQA: F401
 
 
 class SourceService(Service):
