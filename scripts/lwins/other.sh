1b3ec483edb1b739ed8acfaa239bec6c972f8ee9
Alexander <akochnev66@gmail.com>
2026-03-18 01:12:18 +0300

    ammend

--- scripts/lwins/other.sh
+++ scripts/lwins/other.sh
@@ -7,11 +7,11 @@ SERVICE_NAME="test-wine_host-1"
 # docker exec $SERVICE_NAME psql -U wine -d wine_db -c "SELECT DISTINCT l.region, c.id FROM lwins l JOIN countries c ON l.country = c.name WHERE l.region IS NOT NULL AND l.region <> ''"
 # вставляем записи в region
 docker exec -i $SERVICE_NAME psql -U wine -d wine_db -c "
-INSERT INTO producertitle (name)
+INSERT INTO producertitles (name)
 SELECT DISTINCT producer_title FROM lwins
 WHERE producer_title IS NOT NULL AND producer_title <> ''
 ON CONFLICT (name) DO NOTHING;"
 # проверяем
-docker exec -i $SERVICE_NAME psql -U wine -d wine_db -c "SELECT id, name FROM producer_title"
+docker exec -i $SERVICE_NAME psql -U wine -d wine_db -c "SELECT id, name FROM producertitles"
 
 # docker exec -i test-wine_host-1 psql -U wine -d wine_db -c ""
