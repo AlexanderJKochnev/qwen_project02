a412ed7ca5d34131c2887e4f25768f795dc56d7f
Alexander <akochnev66@gmail.com>
2026-03-21 07:53:48 +0300

    producer selectinload -> joinedload

--- app/support/producer/repository.py
+++ app/support/producer/repository.py
@@ -1,34 +1,50 @@
 # app/support/producer/repository.py
 # from sqlalchemy import select, exists
+from sqlalchemy import select, exists
+from sqlalchemy.orm import joinedload, load_only
 from app.core.repositories.sqlalchemy_repository import Repository
-# from app.support.item.model import Item
-# from app.support.drink.model import Drink
+from app.core.utils.alchemy_utils import get_field_list
 from app.support.producer.model import Producer, ProducerTitle
+from app.support import Drink, Item
 
 
 class ProducerTitleRepository(Repository):
     model = ProducerTitle
 
-    """
     @classmethod
     def item_exists(cls, id: int):
         return exists().where(
             Drink.id == Item.drink_id,
-            Producer.id == Drink.producer_id,
+            Drink.producer_id == Producer.id,
             Producer.producertitle_id == id
         )
-    """
 
 
 class ProducerRepository(Repository):
     model = Producer
 
-    """
     @classmethod
     def item_exists(cls, id: int):
         return exists().where(
             Drink.id == Item.drink_id,
-            Producer.id == Drink.producer_id,
-            Producer.producertitle_id == id
+            Drink.producer_id == id
         )
-    """
+
+    @classmethod
+    def get_query(cls, model: Producer):
+        # Добавляем загрузку связи с relationships
+        return select(Producer).options(joinedload(Producer.producertitle))
+
+    @classmethod
+    def get_short_query(cls, model: Producer, field1: tuple = ('id', 'name', 'producertitle_id')):
+        """
+            Возвращает список модели только с нужными полями остальные None
+            - использовать для list_view и вообще где только можно.
+            для моделей с зависимостями - переопределить
+        """
+
+        fields = get_field_list(model, starts=field1)
+        subcat = get_field_list(ProducerTitle, starts=field1)
+        return select(model).options(
+            joinedload(model.producertitle).load_only(*subcat), load_only(*fields)
+        )
