9e0b1b6c71a8534b935faebc90ac5d69f34d09c6
Alexander <akochnev66@gmail.com>
2026-03-21 07:20:03 +0300

    subregion selectinload -> joinedload

--- app/support/subregion/repository.py
+++ app/support/subregion/repository.py
@@ -1,9 +1,11 @@
 # app/support/subregion/repository.py
 
 from sqlalchemy import select, exists
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload, joinedload, load_only
+
 from app.core.repositories.sqlalchemy_repository import ModelType, Repository
-from app.support import Site, Drink, Item, Region, Subregion
+from app.core.utils.alchemy_utils import get_field_list
+from app.support import Site, Drink, Item, Region, Subregion, Country
 
 
 class SubregionRepository(Repository):
@@ -22,3 +24,19 @@ class SubregionRepository(Repository):
         # Добавляем загрузку связи с relationships
         return select(Subregion).options(selectinload(Subregion.region).
                                          selectinload(Region.country))
+
+    @classmethod
+    def get_short_query(cls, model: Region, field1: tuple = ('id', 'name', 'country_id', 'region_id')):
+        """
+            Возвращает список модели только с нужными полями остальные None
+            - использовать для list_view и вообще где только можно.
+            для моделей с зависимостями - переопределить
+        """
+
+        fields = get_field_list(model, starts=field1)
+        subcat = get_field_list(Country, starts=field1)
+        tier3 = get_field_list(Region, starts=field1)
+        return select(model).options(
+            joinedload(model.region).load_only(*tier3).joinedload(Region.country).load_only(*subcat),
+            load_only(*fields)
+        )
