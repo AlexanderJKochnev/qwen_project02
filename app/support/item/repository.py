6f3a7f107a7e8d30f512abd274a1ca139a921f8b
Alexander <akochnev66@gmail.com>
2026-03-22 10:32:35 +0300

    drink repair

--- app/support/item/repository.py
+++ app/support/item/repository.py
@@ -33,7 +33,9 @@ class ItemRepository(Repository):
 
     @classmethod
     def get_query(cls, model: ModelType):
-        query = select(Item).options(
+        subquery = DrinkRepository.get_query(Drink)
+        query = select(Item).options(subquery)
+        query1 = select(Item).options(
             selectinload(Item.drink).options(
                 selectinload(Drink.site).selectinload(Site.subregion).selectinload(Subregion.region).selectinload(
                     Region.country
