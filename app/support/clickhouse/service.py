10eb246c9cdc3ac672460582c0f01e020d512fe7
Alexander <akochnev66@gmail.com>
2026-04-12 00:17:58 +0300

    TextEmbedding.add_custom_model app/support/clickhouse/service.py

--- app/support/clickhouse/service.py
+++ app/support/clickhouse/service.py
@@ -1,43 +1,29 @@
 # app.support.clickhouse.service.py
 from fastembed import TextEmbedding
+from fastembed.common.model_description import PoolingType
 from loguru import logger
 
 
 class EmbeddingService:
     def __init__(self):
-        # Используем существующую регистрацию
-        # Если модель "уже зарегистрирована", просто инициализируем её.
-        # Если возникнет прошлая ошибка ValueError: Model... not supported,
-        # используем обходной путь через загрузку конкретной модели.
+        # Регистрируем твой локальный ONNX под любым именем
+        self.model_name = "fast-e5-local"
         try:
-            self.model = TextEmbedding(
-                model_name="intfloat/multilingual-e5-small",
-                cache_dir="/app/onnx",
-                # Параметр local_files_only=True заставит искать в кэше,
-                # если вы не хотите лезть в сеть
+            TextEmbedding.add_custom_model(
+                model=self.model_name,
+                pooling=PoolingType.MEAN,
+                normalization=True,
+                dim=384,
+                # Указываем путь к папке, которую создали на Шаге 1
+                sources={"local": "./my_e5_onnx_ready"},
+                model_file="onnx/model.onnx"
             )
-        except ValueError as e:
-            logger.error(f'редкий хак для FastEmbed BAAI/bge-small-en-v1.5 {e}')
-            # Если всё равно ругается на "not supported",
-            # инициализируем через ближайший официально поддерживаемый ID,
-            # но подменяя саму модель (редкий хак для FastEmbed)
-            self.model = TextEmbedding(
-                model_name="BAAI/bge-small-en-v1.5",  # Любая поддерживаемая с 384d
-                cache_dir="/app/onnx"
-            )
-            # Принудительно меняем путь к модели, если нужно (но обычно try выше срабатывает)
+            self.model = TextEmbedding(model_name=self.model_name)
+        except Exception as e:
+            logger.error(f'TextEmbedding {e}')
 
     def get_query_embedding(self, query: str):
-        # Префикс ОБЯЗАТЕЛЕН
-        # prefixed_query = f"query: {query}"
-        # # На выходе 384 измерения
-        # embeddings = list(self.model.embed([prefixed_query]))
-        # return embeddings[0].tolist()
-        clean_query = query
-
-        # Генерируем вектор
-        embeddings = list(self.model.embed([clean_query]))
+        # Поскольку в базе НЕТ префиксов, здесь их тоже НЕ ставим
+        # Это вернет Similarity ~1.0 для точных совпадений
+        embeddings = list(self.model.embed([query]))
         return embeddings[0].tolist()
-
-
-embedding_service = EmbeddingService()
