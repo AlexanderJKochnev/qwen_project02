9aa9ba6aac98de1b516f0ffb00488dcf3cc519fe
Alexander <akochnev66@gmail.com>
2026-04-13 15:04:50 +0300

    ammend

--- app/migration/env.py
+++ app/migration/env.py
@@ -36,6 +36,7 @@ from app.support.lwin.model import Lwin
 from app.support import Source
 from app.support.producer.model import Producer, ProducerTitle
 from app.support.parcel.model import Site, Parcel
+from app.support.hashing.model import WordHash
 
 
 # this is the Alembic Config object, which provides
