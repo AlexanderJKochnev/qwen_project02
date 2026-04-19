1d3146164bde2bd0ababe24561d117ec19c5242f
Alexander <akochnev66@gmail.com>
2026-04-19 13:16:45 +0300

    ammned

--- app/support/wordhash/router.py
+++ app/support/wordhash/router.py
@@ -1,14 +1,10 @@
 # app/support/wordhash/router.py
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import BackgroundTasks
 from app.core.routers.base import BaseRouter
 from app.core.config.database.db_async import DatabaseManager
 from app.support.wordhash.model import WordHash
 from app.support.wordhash.repository import WordHashRepository
 from app.support.wordhash.service import WordHashService
-
-
-class BackgroundTasks:
-    pass
 
 
 class WordHashRouter(BaseRouter):
@@ -26,9 +22,8 @@ class WordHashRouter(BaseRouter):
                                   openapi_extra={'x-request-schema': None})
         super().setup_routes()
 
-    async def rebuild_wordhash(self, background_tasks: BackgroundTasks,
-                               session: AsyncSession=Depends(get_db)) -> dict:
+    async def rebuild_wordhash(self, background_tasks: BackgroundTasks) -> dict:
         # Запускает полный пересчет всех хэшей в фоне
-        await self.service.rebuild_all_hashes(background_tasks, DatabaseManager.session_maker, session)
+        await self.service.rebuild_all_hashes(background_tasks, DatabaseManager.session_maker)
         # await WordHashService._run_rebuild_stream(session_factory=session_factory, background_tasks=background_tasks)
         return {"status": "queued", "message": "Пересчет хэшей запущен"}
