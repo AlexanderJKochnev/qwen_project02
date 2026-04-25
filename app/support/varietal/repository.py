25839e62940089ae87fd9970bd8360e85474fb1e
Alexander <akochnev66@gmail.com>
2026-04-25 23:12:43 +0300

    ammend

--- app/support/varietal/repository.py
+++ app/support/varietal/repository.py
@@ -1,9 +1,12 @@
 # app/support/varietal/repository.py
+from typing import Optional, Tuple
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, exists
 from sqlalchemy.orm import selectinload
 from app.support.varietal.model import Varietal
 from app.support.drink.model import DrinkVarietal, Drink
 from app.support.item.model import Item
+from app.core.exceptions import AppBaseException
 from app.core.repositories.sqlalchemy_repository import Repository, ModelType
 
 
@@ -36,3 +39,29 @@ class VarietalRepository(Repository):
     def get_query(cls, model: ModelType):
         # Добавляем загрузку связи с relationships
         return select(cls.model).options(selectinload(Varietal.drink_associations).joinedload(DrinkVarietal.drink))
+
+    @classmethod
+    async def get_by_ids(cls, ids: Tuple[int], model: ModelType, session: AsyncSession) -> Optional[ModelType]:
+        """
+            get records by ids tuple
+        """
+        stmt = select(model).where(model.id.in_(ids)).order_by(model.id.asc())
+        # from sqlalchemy.dialects import postgresql
+        # compiled_pg = stmt.compile(dialect=postgresql.dialect(), compile_kwargs={"literal_binds": True})
+        # logger.error(compiled_pg)
+        result = await cls.nonpagination(stmt, session)
+        return result
+
+    @classmethod
+    async def get_full_with_pagination(
+            cls, skip: int, limit: int, model: ModelType, session: AsyncSession, ) -> tuple:
+        """
+            Запрос полного списка с загрузкой связей и пагинацией
+            return Tuple[List[instances], int]
+        """
+        try:
+            stmt = select(model).order_by(model.id.asc())
+            result = await cls.pagination(stmt, skip, limit, session)
+            return result
+        except Exception as e:
+            raise AppBaseException(message=f'get_full_with_pagination.error; {str(e)}', status_code=404)
