6595905fe84d8b5708e04f1dc8af543718c6ade5
Alexander <akochnev66@gmail.com>
2026-04-26 21:09:08 +0300

    ammend

--- preact_front/src/forms/fields/LazyCheckboxGroupField.ts
+++ preact_front/src/forms/fields/LazyCheckboxGroupField.ts
@@ -156,7 +156,7 @@ const LazyCheckboxCore = ({ name, label, value, loadOptions, getDisplayName, ren
                 id: `${name}-${optionId}`,
                 checked: isChecked,
                 onChange: (e: any) => handleToggle(optionId, e.target.checked),
-                className: 'checkbox checkbox-primary checkbox-sm mr-2'
+                className: 'mr-2'
               }),
               h('label', {
                 htmlFor: `${name}-${optionId}`,
