07aa55b668c043df32aa8a7788f165102c7f87c5
Alexander <akochnev66@gmail.com>
2026-04-26 16:50:17 +0300

    test ammend

--- preact_front/vite.config.ts
+++ preact_front/vite.config.ts
@@ -1,8 +1,6 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
-// Считываем порт из переменных окружения. Если его нет — берем 5173
-const PORT = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
 
 export default defineConfig({
   plugins: [preact()],
@@ -14,10 +12,10 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: PORT,
+    port: 5173,
     strictPort: true,
     hmr: {
-      port: PORT,
+      port: 5173,
       host: '0.0.0.0',
     },
     proxy: {
@@ -25,12 +23,6 @@ export default defineConfig({
         target: 'http://app:8091',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/proxy-api/, ''),
-        // ДОБАВЬТЕ ЭТОТ БЛОК ДЛЯ ОТОБРАЖЕНИЯ ЛОГОВ В ТЕРМИНАЛЕ 👇
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log(`[Proxy] Отправка запроса: ${req.method} ${req.url}`);
-          });
-        },
       },
     },
   },
