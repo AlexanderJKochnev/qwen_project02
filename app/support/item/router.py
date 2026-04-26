4b9d071d92281b0dadbb479ed2617879c7fb7504
Alexander <akochnev66@gmail.com>
2026-04-26 18:20:59 +0300

    ammend

--- app/support/item/router.py
+++ app/support/item/router.py
@@ -335,7 +335,5 @@ class ItemRouter(BaseRouter):
                                  media_type=image_data['content_type'],
                                  headers=headers)
         """
-        from app.core.utils.common_utils import jprint
-        jprint(headers)
-        logger.warning('===============================================')
+
         return ResponseJust(image_data, headers)
