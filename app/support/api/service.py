4291e94b3d4757d2ab46a14a0c813b938a044da2
Alexander <akochnev66@gmail.com>
2026-05-03 11:34:58 +0300

    prive

--- app/support/api/service.py
+++ app/support/api/service.py
@@ -271,7 +271,10 @@ class ApiService(ItemService):
                                    penalty: float = 0.1):
         # items = await super().execute_smart_search(query, session, boost, limit)
         logger.warning(f'in {query=}')
+        raw = query
         query = query.replace('+', ' ')
         logger.warning(f'out {query=}')
         items: List[dict] = await super().search_by_hash(query, Item, ItemRepository, session, limit, boost, penalty)
-        return [transform_api_list_view(item, def_lang, cls.lang_suffix_list(language)) for item in items]
+        result = [transform_api_list_view(item, def_lang, cls.lang_suffix_list(language)) for item in items]
+        logger.warning(f'{raw=}.cleared={query} count={len(result)}')
+        return result
