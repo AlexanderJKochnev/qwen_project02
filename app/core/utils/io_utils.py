2f71621993b9712c46033a9bdb6c15fc8884a385
Alexander <akochnev66@gmail.com>
2026-05-18 16:34:55 +0300

    ammend

--- app/core/utils/io_utils.py
+++ app/core/utils/io_utils.py
@@ -101,13 +101,16 @@ def ResponseStreaming(image_data: dict, headers: dict):
 
 def ResponseStreaming2(content: bytes, headers: dict):
     # media_type, content_type, mime_type
+    return Response(content=content,
+                    media_type=headers.get("Content-Type"),
+                    headers=headers)
+    """
     return StreamingResponse(
         BytesIO(content),
-        # media_type=image_data.get('content_type'),
         media_type=headers.get("Content-Type"),
         headers=headers
     )
-
+    """
 
 def ResponseJust(image_data: dict, headers: dict):
     # from app.core.utils.common_utils import jprint
