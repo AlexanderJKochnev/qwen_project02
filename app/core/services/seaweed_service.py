b07f42abde3425009bacf40adcf721404aa9f3e3
Alexander <akochnev66@gmail.com>
2026-05-19 10:21:24 +0300

    ammend

--- app/core/services/seaweed_service.py
+++ app/core/services/seaweed_service.py
@@ -162,7 +162,7 @@ class SeaweedsService:
         """
             получение изображения по fid (любого)
         """
-        image_url = settings.seaweed_url
+        image_url = f'{settings.seaweed_url}/{fid}'
         logger.info(f'{image_url=}')
         async with aiohttp.ClientSession() as client:
             async with client.get(image_url) as response:
