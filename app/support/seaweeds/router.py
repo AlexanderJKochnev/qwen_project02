1ef8be091d1f982e4fa6f5a8ce36b914526e2e43
Alexander <akochnev66@gmail.com>
2026-05-26 13:58:43 +0300

    two image_generators in seaweed router
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/seaweeds/router.py
+++ app/support/seaweeds/router.py
@@ -375,6 +375,7 @@ class SeaweedsRouter:
         """
         # fill_color = color_converter(COLORS.get(fill_color), fill_opacity)  # RGBA
         background_color = color_converter(COLORS.get(background_color), bg_opacity)  # RGBA
+        logger.warning(f'{background_color=}, {COLORS.get(background_color)=}')
         # stroke_color = color_converter(COLORS.get(stroke_color), 255)
         # shadow_color = color_converter(COLORS.get(shadow_color), shadow_opacity)
         if shadow_x != 0 or shadow_y != 0:
