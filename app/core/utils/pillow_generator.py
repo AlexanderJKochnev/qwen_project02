54c8c9839b9a1d1b9e8ebf98e9024187b18c7301
Alexander <akochnev66@gmail.com>
2026-05-26 11:03:50 +0300

    IMAGE GENERATOR IN items_view router
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/core/utils/pillow_generator.py
+++ app/core/utils/pillow_generator.py
@@ -41,6 +41,7 @@ class TextConfig:
         """Автоматически подмешивает прозрачность к цветам после создания объекта."""
         self.fill_color = tuple([*self.fill_color[:3], self.fill_opacity])
         self.shadow_color = tuple([*self.shadow_color[:3], self.shadow_opacity])
+        self.text = __import__('functools').reduce(lambda t, x: t.replace(f'{x} ', '\n'), ',.;:', self.text)
 
 
 def should_allow_single_word(word: str, min_length: int) -> bool:
